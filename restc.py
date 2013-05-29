eb9b8db8c878941450f3a14dde47e4fe29a29e8f
Edgar Landivar <elandivar@palosanto.com>
2013-05-28 19:24:09 -0500

    Filling 'data' with an empty array when the HTTP response is not 200. This way we can avoid errors when handling this variable.
    Before this change, when an error ocurred (eg. authentication error) the
    application stopped.

--- restc.py
+++ restc.py
@@ -3,12 +3,19 @@ import httplib2 #installed manually
 
 def get_rest_data(http, url_ext, url_int):
    # Retrieve External Contact List
-   response, content = http.request(url_ext, 'GET', body='', headers={'content-type':'text/plain'})
+   response1, content = http.request(url_ext, 'GET', body='', headers={'content-type':'text/plain'})
+   print response1.status
    data1 = json.loads(content.decode())
    # Retrieve Internal Contact List
-   response, content = http.request(url_int, 'GET', body='', headers={'content-type':'text/plain'})
+   response2, content = http.request(url_int, 'GET', body='', headers={'content-type':'text/plain'})
    data2 = json.loads(content.decode())
    data = {}
-   data['contacts'] = data1['contacts']
-   data['extensions'] = data2['extension']
+   # If HTTP status is not 200 fill data with an empty array to avoid errors when handle the data variable
+   if response1 == 200 and response2 == 200:
+      data['contacts'] = data1['contacts']
+      data['extensions'] = data2['extension']
+   else:
+      data['contacts'] = {}
+      data['extensions'] = {}
+   
    return data
